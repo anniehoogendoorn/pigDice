e3bd979d983800bcad0fc68c9210091c8dabcbe5
Annie Hoogendoorn & Austin Blanchard <student@epicodus.com>
2015-09-16 15:09:42 -0700

    Add jquery for Roll Total display.

--- js/scripts.js
+++ js/scripts.js
@@ -11,11 +11,25 @@ Turn.prototype.diceRoller = function() {
     var randNumber = Math.floor(Math.random() * 6) + 1;
     console.log(randNumber);
     this.total += randNumber;
-    return this.total;
+    return randNumber;
 }
 
+$(document).ready(function() {
+    var newTurn = new Turn();
+    var total = newTurn.total;
+    $("#roll-total").text(total);
 
-// var diceRoll = function() {
-//     // var randomNumber = Math.floor(Math.random)
-//     return Math.floor(Math.random() * 6) + 1;
-// }
+$("form#roll").submit(function(event) {
+    event.preventDefault();
+
+    $('#roll-total').text(newTurn.total);
+
+    var result = newTurn.diceRoller();
+    $('#roll').text(result);
+
+
+
+});
+
+// $("")
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,3 @@
-// describe('DiceRoll', function() {
-//   it("returns a random integer between 1 and 6", function() {
-//     expect(diceroll).to.equal("Rita");
-//   });
-//
-// });
-
 describe('Player', function() {
     it("creates a new player with a name entered by the user", function() {
         var testPlayer = new Player("Piggy");
@@ -19,6 +12,5 @@ describe('Turn', function() {
         var testTurn = new Turn();
         expect(testTurn.diceRoller()).to.be.within(1, 6);
     });
-
 
 });
