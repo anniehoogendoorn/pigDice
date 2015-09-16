647b826d9ca235a44bd7c628ac27ce2990a872b9
Annie Hoogendoorn & Austin Blanchard <student@epicodus.com>
2015-09-16 12:18:00 -0700

    Add diceRoller prototype method and spec.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,16 @@ function Player(userName) {
     this.score = 0;
 }
 
+function Turn() {
+    this.total = 0;
+}
+
+Turn.prototype.diceRoller = function() {
+    var randNumber = Math.floor(Math.random() * 6) + 1;
+    console.log(randNumber);
+    this.total += randNumber;
+    return this.total;
+}
 
 
 // var diceRoll = function() {
--- spec/specs.js
+++ spec/specs.js
@@ -12,3 +12,13 @@ describe('Player', function() {
     });
 
 });
+
+
+describe('Turn', function() {
+    it("returns a random integer between 1 and 6", function() {
+        var testTurn = new Turn();
+        expect(testTurn.diceRoller()).to.be.within(1, 6);
+    });
+
+
+});
