18a70ab938180aeacdf760c3840be13af3533304
Annie Hoogendoorn & Austin Blanchard <student@epicodus.com>
2015-09-16 11:20:26 -0700

    Add Player function contructor and first spec.

--- spec/specs.js
+++ spec/specs.js
@@ -1,28 +1,14 @@
-describe('Contact', function() {
-  it("creates a new contact with the given specifications", function() {
-    var testContact = new Contact("Rita","Moreno");
-    expect(testContact.firstName).to.equal("Rita");
-    expect(testContact.lastName).to.equal("Moreno");
-    expect(testContact.addresses).to.eql([]);
-  });
+// describe('DiceRoll', function() {
+//   it("returns a random integer between 1 and 6", function() {
+//     expect(diceroll).to.equal("Rita");
+//   });
+//
+// });
 
-  it("displays the contacts full name", function() {
-    var testContact = new Contact("Rita","Moreno");
-    expect(testContact.fullName()).to.equal("Rita Moreno");
-  });
+describe('Player', function() {
+    it("creates a new player with a name entered by the user", function() {
+        var testPlayer = new Player("Piggy");
+        expect(testPlayer.userName).to.equal("Piggy");
+    });
 
-});
-
-describe('Address', function() {
-  it("creates a new address with the given specifications", function() {
-    var testAddress = new Address("123 Main St", "Test City","Test State");
-    expect(testAddress.street).to.equal("123 Main St");
-    expect(testAddress.city).to.equal("Test City");
-    expect(testAddress.state).to.equal("Test State");
-  });
-
-  it("adds the fullAddress method to all addresses", function() {
-    var testAddress = new Address("123 Main St", "Test City","Test State");
-    expect(testAddress.fullAddress()).to.equal("123 Main St, Test City, Test State");
-  });
 });
